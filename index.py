e53e0169ce709aebcb27b400c6d691bfd7fe23f0
zcjtshy <see.yim.13@ucl.ac.uk>
2017-11-03 09:24:50 +0000

    add .

--- index.py
+++ index.py
@@ -7,6 +7,26 @@ app = Flask("MyApp") #import  Setting up the internal of the web server, app is 
 def hello():   
 	return "Hello World"
 
+
+@app.route("/request_api")
+
+def exchange_code(code):
+	API_ENDPOINT = 'https://api.instagram.com/oauth/access_token'
+	CLIENT_ID = '58afe6a207db45a0a4c709a6ce8fff0a'
+	CLIENT_SECRET = '1d846b34d9b743b5b68d303e41eeeb57'
+	REDIRECT_URI = 'https://food-print.herokuapp.com'
+	data = {
+    	'client_id': CLIENT_ID,
+    	'client_secret': CLIENT_SECRET,
+    	'grant_type': 'authorization_code',
+    	'code': 'eccd0dee29284a7892de22ff9d98d81b',
+    	'redirect_uri': REDIRECT_URI
+ 	}
+  	print response.headers["content-type":'application/x-www-form-urlencoded']  
+  	r = requests.post('%s/oauth2/token' %API_ENDPOINT, data, headers)
+  	print r.url
+	print r.raise_for_status()
+	return r.json()
 	 
 @app.route("/index/<NumberOfIndex>/<hello>")  
 def index(NumberOfIndex, hello):
